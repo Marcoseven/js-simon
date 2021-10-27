7bb44eeed383bc40066aee1595e3682cd5609a14
Marcoseven <marco.caliciotti3@gmail.com>
2021-10-27 17:52:57 +0200

    Aggiunte modifiche (js)
    Inserite due funzioni, un array, un ciclo for e un condizionale if.

--- assets/main.js
+++ assets/main.js
@@ -12,13 +12,46 @@ Info:
 L'alert non si chiude in automatico. Dovete chiuderla voi a mano cliccando su ok.
 
 STRUMENTI
-- alert
-- setTimeout ?
-- prompt()
-- if
+- ciclo;
+- alert;
+- setTimeout; 
+- prompt();
+- if;
 */
 
-// alert
-const numberRandom = alert(Math.round(Math.random() * 4) + 1);
-console.log(numberRandom);
+// DOM
+const result = document.getElementById("result");
 
+// array
+let numbersRandom = [];
+
+// function e ciclo for
+myFunctions();
+function myFunctions() {
+	for (let index = 0; index < 5; index++) {
+		const getNumbers = Math.round(Math.random() * 100) + 1;
+		numbersRandom.push(getNumbers);
+		console.log(getNumbers);
+	}
+}
+
+// alert
+alert(numbersRandom);
+
+// setTimeout e function
+setTimeout(popUp, 3000);
+popUp();
+function popUp() {
+	return parseInt(
+		prompt("Prova ad inserire i cinque numeri casuali comparsi in precedenza")
+	);
+}
+
+// condizionale if
+if (popUp == numbersRandom) {
+	console.log("Hai indovinato!");
+	result.innerHTML = "Hai indovinato!";
+} else if (popUp != numbersRandom) {
+	console.log("Mi dispiace, non hai indovinato");
+	result.innerHTML = "Mi dispiace, non hai indovinato";
+}
